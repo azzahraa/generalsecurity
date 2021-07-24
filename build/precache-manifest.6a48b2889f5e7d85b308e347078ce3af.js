87270f82412e13333c7b549272f0c9da66de15b8
azzahraa <azzahraa36@gmail.com>
2021-07-24 23:40:21 +0100

    first commit

---
+++ build/precache-manifest.6a48b2889f5e7d85b308e347078ce3af.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b38c4d998d32cc46d3a9908733f548",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8017e20e488c19b27dfc",
+    "url": "/static/css/main.d8271000.chunk.css"
+  },
+  {
+    "revision": "a01947c86935a6aef2f6",
+    "url": "/static/js/2.9a9e3e20.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.9a9e3e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8017e20e488c19b27dfc",
+    "url": "/static/js/main.77f0870c.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  },
+  {
+    "revision": "b23df7d2b42b14f0bb26dc955aaa8fa6",
+    "url": "/static/media/QG.b23df7d2.jpg"
+  },
+  {
+    "revision": "869c7c8420dd6c328d3607e96235f7f8",
+    "url": "/static/media/camnature.869c7c84.png"
+  },
+  {
+    "revision": "0a6e0377110d85d2903d7337afaae3b6",
+    "url": "/static/media/cams.0a6e0377.jpg"
+  },
+  {
+    "revision": "f08056f5cb171f67118f6b92ef926847",
+    "url": "/static/media/camstreet.f08056f5.jpg"
+  },
+  {
+    "revision": "795a638ca3c3d43fa9bd4b2d91f5baaa",
+    "url": "/static/media/cnp.795a638c.jpg"
+  },
+  {
+    "revision": "5a7efe24809873c933d8a85c7224f44a",
+    "url": "/static/media/comet.5a7efe24.png"
+  },
+  {
+    "revision": "8b9f698920f29b4bd071c058ece65444",
+    "url": "/static/media/ecovillage.8b9f6989.jpg"
+  },
+  {
+    "revision": "239ee295a79ef98aa8f71eb2eb7d5b00",
+    "url": "/static/media/header.239ee295.jpeg"
+  },
+  {
+    "revision": "5bc7726e5061d3a65ade179e216826ed",
+    "url": "/static/media/hutchinson.5bc7726e.jpg"
+  },
+  {
+    "revision": "dcf5e11c28f2ecda04d55c1e6555e04f",
+    "url": "/static/media/isuzu.dcf5e11c.jpg"
+  },
+  {
+    "revision": "275dc2a8115e9d293e3d45965fdfb333",
+    "url": "/static/media/lg.275dc2a8.png"
+  },
+  {
+    "revision": "2e153a89300b562e2a53b11d1dc96eff",
+    "url": "/static/media/mirage.2e153a89.jpg"
+  },
+  {
+    "revision": "a92d63c51c04918c565a043c61310af8",
+    "url": "/static/media/permaflex.a92d63c5.png"
+  },
+  {
+    "revision": "ec20c8b701efe3757cea36565a4734ae",
+    "url": "/static/media/steg.ec20c8b7.png"
+  },
+  {
+    "revision": "9db7bf08c28c62fbf842ea11a3c4e1ea",
+    "url": "/static/media/un.9db7bf08.png"
+  },
+  {
+    "revision": "642ad70f3b29b306dc185230ddd64c4f",
+    "url": "/static/media/warda.642ad70f.jpg"
+  }
+]);
